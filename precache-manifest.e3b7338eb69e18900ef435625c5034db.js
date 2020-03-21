642652189e05312567fd703290d38b36b40f3105
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-03-21 15:56:44 +0530

    Updates

---
+++ precache-manifest.e3b7338eb69e18900ef435625c5034db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c14ef999384539a50723f92303eb0e4",
+    "url": "/react-kanban-drag-and-drop/index.html"
+  },
+  {
+    "revision": "df9cbe9aae469f780138",
+    "url": "/react-kanban-drag-and-drop/static/css/main.2a764b1c.chunk.css"
+  },
+  {
+    "revision": "5bd5220e660cdaaf4c6b",
+    "url": "/react-kanban-drag-and-drop/static/js/2.706a221e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-kanban-drag-and-drop/static/js/2.706a221e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9cbe9aae469f780138",
+    "url": "/react-kanban-drag-and-drop/static/js/main.fec71e74.chunk.js"
+  },
+  {
+    "revision": "758c837d95d4a0fc1d59",
+    "url": "/react-kanban-drag-and-drop/static/js/runtime-main.8706d51f.js"
+  }
+]);
